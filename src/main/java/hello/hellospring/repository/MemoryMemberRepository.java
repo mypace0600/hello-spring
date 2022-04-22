81cb6a8a136898d13213c28938e6c7c6173ec223
admin <mypace0600@gmail.com>
2022-04-22 12:48:13 +0900

    Test case

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -11,7 +11,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
-        member.setId(sequence);
+        member.setId(++sequence);
         store.put(member.getId(),member);
         return member;
     }
@@ -23,11 +23,16 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+    public void clearStore(){
+        store.clear();
     }
 }
